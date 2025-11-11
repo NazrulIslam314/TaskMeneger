7d6c916c94982ac5c0ef3165a63bd0875e76b1c5
Nazrul Islam <nazrulislam3144@gmail.com>
2025-11-11 21:17:53 +0600

    added Fragment & arrange the project file structure

--- app/src/main/java/com/nazulislam/taskmeneger/start/presentation/MainActivity.kt
+++ app/src/main/java/com/nazulislam/taskmeneger/start/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.nazulislam.taskmeneger
+package com.nazulislam.taskmeneger.start.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.nazulislam.taskmeneger.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
